e73d32e70ceacc585363611521bf1e80da671ca4
vosem <progpabsanch@gmail.com>
2025-06-04 17:57:44 +0200

    feat: new design and start poetry

--- vosem.py
+++ vosem.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run()
