960a5d73526648e753c3d16a0966d0f327d79d61
vosem <progpabsanch@gmail.com>
2025-06-11 12:56:25 +0200

    feat: standardize date format

--- vosem.py
+++ vosem.py
@@ -2,15 +2,22 @@ from flask import Flask, render_template
 import os
 from exif import Image
 from geopy import Nominatim
+from dateutil.parser import parse
 
 app = Flask(__name__)
 
+
 def get_locale(latitude, longitude):
-    dlat = (-1 if latitude[4] == "S" else 1)* (latitude[0] + latitude[1] / 60 + latitude[2] / 3600)
-    dlon = (-1 if longitude[4] == "W" else 1)* (longitude[0] + longitude[1] / 60 + longitude[2] / 3600)
+    dlat = (-1 if latitude[4] == "S" else 1) * (
+        latitude[0] + latitude[1] / 60 + latitude[2] / 3600
+    )
+    dlon = (-1 if longitude[4] == "W" else 1) * (
+        longitude[0] + longitude[1] / 60 + longitude[2] / 3600
+    )
 
     gl = Nominatim(user_agent="GetLoc")
     return gl.reverse(f"{dlat}, {dlon}")
+
 
 def get_file_meta(filepath):
     with open(filepath, "rb") as photof:
@@ -19,13 +26,22 @@ def get_file_meta(filepath):
     if photoi.has_exif:
         locale = None
         if photoi.get("gps_latitude", "--") != "--":
-            locale = get_locale(photoi.gps_latitude + [photoi.gps_latitude_ref], photoi.gps_longitude + [photoi.gps_longitude_ref])
+            locale = get_locale(
+                photoi.gps_latitude + [photoi.gps_latitude_ref],
+                photoi.gps_longitude + [photoi.gps_longitude_ref],
+            )
 
-        return photoi.get("image_description", "--"), photoi.get("datetime_original", "--"), locale or "--"
+        photodate = None
+        if photoi.get("datetime_original", "--") != "--":
+            photodate = parse(photoi.datetime_original).strftime("%d/%m/%Y %H:%M:%S")
+
+        return photoi.get("image_description", "--"), photodate or "--", locale or "--"
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/photos")
 def photos():
@@ -46,6 +62,7 @@ def photos():
             }
         )
     return render_template("photos.html", photos=photos)
+
 
 if __name__ == "__main__":
     app.run()
